1e9fd743e65dd33473b05073c94dfd2f4cd18d85
aom <aom@n128-107.sfc.wide.ad.jp>
2013-06-20 01:22:25 +0900

    src tcp

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <errno.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 int main(){
+    
+    struct hostent *host;
+    struct sockaddr_in me;
+    int s;
+    char buf[512];
+    s = socket(PF_INET, SOCK_STREA<,0);
+    bzero((char *)&me,sizeof(me));
+    me.sin_family = PF_INET;
+    me.sin_port = htons(port);
 
+    if(bind(s,(struct sockaddr *)&me. sizeof(me))>0){
+	fprintf(stderr, "cannot bind socket\n");
+	exit(1);
+    }
+    listen(s,1);
+    sock = accept(s,NULL,NULL);
 }
 
