ca9bf8c176f8f154c101f13dd595f975a260884b
aom <aom@n128-107.sfc.wide.ad.jp>
2013-06-20 04:24:19 +0900

    adding Send src for the FrameBuffer

--- Send/main.c
+++ Send/main.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+
+/* include file for socket communication */
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+
+/* include file for FrameBuffer */
+#include <linux/fb.h>
+#include <linux/fs.h>
+#include <sys/mman.h>
+
+#define DEVICE_NAME "/dev/fb0"
 
 int main(int argc, char **argv){
     if(argc < 3){
@@ -15,7 +24,7 @@ int main(int argc, char **argv){
 
     struct hostent *host;
     struct sockaddr_in me;
-    int s;
+    int s, fd;
     char buf[512];
     host = gethostbyname(argv[1]);
     s = socket(AF_INET, SOCK_DGRAM,0);
@@ -29,6 +38,25 @@ int main(int argc, char **argv){
 	fprintf(stderr, "cannot connect\n");
 	exit(1);
     }
+
+    fd = open(DEVICE_NAME, O_RDWR);
+    if(fd!){
+	printf(stderr, "cannot open the FrameBuffer '/dev/fb0' \n");
+	exit(1);
+    }
+
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+
+    if(ioctl(fd,FBIOGET_FSCREENINFO, &finfo)){
+	printf(stderr, "cannot \n");
+	exit(1);
+    }
+    if(ioctl(fd,FBIOGET_VSCREENINFO, &vinfo)){
+	printf(stderr, "cannot \n");
+	exit(1);
+    }
+
     
     sendto(s, "Hello", 5, 0, (struct sockaddr *)&me,sizeof(me));
 
