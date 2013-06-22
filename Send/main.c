9084362453a2cc2b5b6f1da21b5e1e23950dfeea
aom <aom@n128-107.sfc.wide.ad.jp>
2013-06-23 05:24:44 +0900

    adding

--- Send/main.c
+++ Send/main.c
@@ -1,3 +1,5 @@
+
+/* include file for standard library for C */
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -13,18 +15,22 @@
 #include <linux/fs.h>
 #include <sys/mman.h>
 
+/* define the Parameter */
 #define DEVICE_NAME "/dev/fb0"
 
 int main(int argc, char **argv){
+
+    /*check the augment */
     if(argc < 3){
 	fprintf(stderr, "usage : ./a.out <hostname> <port>\n");
 	exit(1);
     }
-    int port = atoi(argv[2]);
 
+    /* open network socket */
+    int port = atoi(argv[2]);
+    int s;
     struct hostent *host;
     struct sockaddr_in me;
-    int s, fd;
     char buf[512];
     host = gethostbyname(argv[1]);
     s = socket(AF_INET, SOCK_DGRAM,0);
@@ -38,6 +44,9 @@ int main(int argc, char **argv){
 	fprintf(stderr, "cannot connect\n");
 	exit(1);
     }
+
+    /* FrameBuffer */
+    int fd, screensize;
 
     fd = open(DEVICE_NAME, O_RDWR);
     if(fd!){
@@ -56,11 +65,32 @@ int main(int argc, char **argv){
 	printf(stderr, "cannot \n");
 	exit(1);
     }
+    int xres,yres,bpp,line_len;
+    xres = vinfo.xres;  yres = vinfo.yres;  bpp = vinfo.bits_per_pixel; 
+    line_len = finfo.line_length;
+printf("%d(pixel)x%d(line), %d(bit per pixel), %d(line length)\n",xres,yres,bpp,line_len);
+    screensize = xres * yres * bpp;
 
+    /*memory I/O */
+    char *fbptr;
+
+    fbptr = (char *)mmap(0,screensize,PROT_READ | PROT_WRITE, MAP,fd,0);
+    if((int)fbptr == -1){
+	printf(stderr,"cannot get framebuffer\n");
+	exit(1);
+    }
     
+printf("the frame buffer device was mapped\n");
+    munmap(fbptr,screensize);
+    close(fd);
+
+
+    /* sending a packet on UDP */
     sendto(s, "Hello", 5, 0, (struct sockaddr *)&me,sizeof(me));
 
+    /* close the filediscriptor of socket */
     close(s);
+
 
     return 0;
 }
